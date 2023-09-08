db278d6c1b0616d7b9d9679caca74cdafa8d4526
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-09-08 09:18:48 -0600

    Debugging CMake against local stack.

--- modulefiles/build.hera.intel.lua
+++ modulefiles/build.hera.intel.lua
@@ -7,7 +7,7 @@ prepend_path("MODULEPATH", "/scratch1/NCEPDEV/da/Henry.Winterbottom/UFS/spack-st
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.0"
+hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
 load(pathJoin("intel", hpc_intel_ver))
 
 impi_ver=os.getenv("impi_ver") or "2022.1.2"
