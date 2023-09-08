503f02e5c67a41fab46a0c3805ce7ef1f3f8934f
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-09-08 08:57:38 -0600

    Debugging CMake against local stack.

--- modulefiles/build.hera.intel.lua
+++ modulefiles/build.hera.intel.lua
@@ -5,7 +5,7 @@ Load environment to compile UFS_UTILS on NOAA CSPs using Intel
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.0"
+hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
 load(pathJoin("intel", hpc_intel_ver))
 
 impi_ver=os.getenv("impi_ver") or "2022.1.0"
